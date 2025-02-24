9f48d5a8b506028d028937571af30d4759973eab
whiskas101 <getintouchwithvic@gmail.com>
2025-02-24 18:03:57 +0530

    big change

---
+++ move-media.sh
@@ -0,0 +1,20 @@
+
+move_media(){
+	MEGAPATH=$HOME/storage/downloads/MEGA\ Downloads	
+	REDDITPATH=$HOME/storage/downloads/Pictures/Reddit
+	DWNLDPATH=$HOME
+
+	ls `$MEGAPATH`
+	ls `$REDDITPATH`
+
+}
+
+move_media
+
+
+
+
+
+
+
+
