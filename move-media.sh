49f5891c36809029a8453db6e1e154fd87160606
whiskas101 <getintouchwithvic@gmail.com>
2025-02-24 18:44:49 +0530

    update /r

--- move-media.sh
+++ move-media.sh
@@ -1,12 +1,17 @@
 
 move_media(){
 	MEGAPATH=$HOME/storage/downloads/MEGA\ Downloads
-	REDDITPATH=$HOME/storage/downloads/Pictures/Reddit
+	REDDITPATH=$HOME/storage/shared/Pictures/Reddit
 	DWNLDPATH=$HOME
+	
+	DEST=$HOME/storage/shared/staging-area
+	#ls `$MEGAPATH`
+	#ls `$REDDITPATH`
 
-	ls `$MEGAPATH`
-	ls `$REDDITPATH`
-
+	# match images and videos and move them to the staging-area folder
+	
+	# " . " char needs to be escaped in egrep because it is treated as special by default
+	find "$HOME" | egrep "*\.txt|*\.lua"
 }
 
 move_media
