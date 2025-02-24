5f85a598084b83cd9301f74180b5ab7e06aff116
whiskas101 <getintouchwithvic@gmail.com>
2025-02-24 19:25:07 +0530

    updated move-media.sh

--- move-media.sh
+++ move-media.sh
@@ -11,7 +11,17 @@ move_media(){
 	# match images and videos and move them to the staging-area folder
 	
 	# " . " char needs to be escaped in egrep because it is treated as special by default
-	find "$HOME" | egrep "*\.txt|*\.lua"
+	echo "Moving files from MEGA..."
+	find "$MEGAPATH" -type f -print0 | egrep -z "*\.txt|*\.mp4| *\.mkv|*\.mov|*\.avi|*\.webm|*\.png|*\.jpg|*\.jpeg|*\.webp" | xargs -0 -I {} mv {} "$DEST/{}"
+
+	echo "Moving files from REDDIT..."
+	find "$REDDITPATH" -type f -print0 | egrep -z "*\.txt|*\.mp4| *\.mkv|*\.mov|*\.avi|*\.webm|*\.png|*\.jpg|*\.jpeg|*\.webp" | xargs -0 -I {} mv {} "$DEST/{}"
+
+	echo "Moving files from Downloads..."
+	find "$DWNLDPATH" -type f -print0 | egrep -z "*\.txt|*\.mp4| *\.mkv|*\.mov|*\.avi|*\.webm|*\.png|*\.jpg|*\.jpeg|*\.webp" | xargs -0 -I {} mv {} "$DEST/{}"
+
+	echo "Finished"
+	exit
 }
 
 move_media
